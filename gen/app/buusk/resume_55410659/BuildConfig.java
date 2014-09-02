a7c8c86c9a65a80715fd971f424997e9e37cbcef
Yaowapha <saisin.2536@gmail.com>
2014-09-02 19:16:12 +0700

    fha

---
+++ gen/app/buusk/resume_55410659/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.buusk.resume_55410659;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/buusk/resume_55410659/ImageActivity.java
@@ -0,0 +1,14 @@
+package app.buusk.resume_55410659;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_yaowpha);
+	}
+
+}
